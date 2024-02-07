01713f6d7823ab7bb69698fc9ce8864b72a65e25
FedericaSidoti <federicasidoti21@gmail.com>
2024-02-07 23:41:51 +0100

    FIX- general style and header, fontfamily

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script>
-import Appheader from './components/Appheader.vue';
+import Appheader from './components/AppHeader.vue';
 
 export default {
     components: {
@@ -10,26 +10,44 @@ export default {
 
 <template>
     <Appheader class="z-ind"/>
-      <router-view class="main">
-      </router-view>
+    <router-view class="main" v-slot="{ Component }">
+        <transition name="slide">
+          <component :is="Component" />
+        </transition>
+    </router-view>
 
 </template>
 
 <style lang="scss">
 @use './styles/general.scss';
 
-.main {
-  padding: 50px; 
-  min-height: 100vh;
+
+.slide-enter-active,
+.slide-leave-active {
+  transition: all 0.3s ease-out;
 }
 
-.z-ind {
-  position: sticky;
-  z-index: 100; 
+.slide-enter-to {
+  position: absolute;
+  right: 0;
+}
+
+.slide-enter-from {
+  position: absolute;
+  right: -100%;
+}
+
+.slide-leave-to {
+  position: absolute;
+  left: -100%;
+}
+
+.slide-leave-from {
+  position: absolute;
+  left: 0;
 }
 
 
 
-
-@import url('https://fonts.googleapis.com/css2?family=Chakra+Petch:wght@300;500;700&family=Dosis:wght@300;500;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;700&display=swap');
 </style>
