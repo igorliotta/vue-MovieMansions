6bc58a11cfd22cb104b0f56abef58cef1812dcb1
NicolaSforza93 <nicola.sforza93@gmail.com>
2024-03-10 12:43:39 +0100

    fix section-building

--- src/views/buildings/Index.vue
+++ src/views/buildings/Index.vue
@@ -345,15 +345,7 @@ export default {
                         :key="building.id" />
                 </div>
                 <div class="loading" v-else>
-                    <!-- <p>Non ci sono risultati</p> -->
-                    <section class="dots-container">
-                        <div class="dot"></div>
-                        <div class="dot"></div>
-                        <div class="dot"></div>
-                        <div class="dot"></div>
-                        <div class="dot"></div>
-                    </section>
-
+                    <p>Non ci sono risultati</p>
                 </div>
                 <div class="text-center mt-4">
                     <button v-if="viewBuildings < store.buildings.length" class="btn load-more-btn" @click="showMore">Mostra
@@ -366,58 +358,7 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-.dots-container {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-  width: 100%;
-}
 
-.dot {
-  height: 20px;
-  width: 20px;
-  margin-right: 10px;
-  border-radius: 10px;
-  background-color: #5B8E81;
-  animation: pulse 1.5s infinite ease-in-out;
-}
-
-.dot:last-child {
-  margin-right: 0;
-}
-
-.dot:nth-child(1) {
-  animation-delay: -0.3s;
-}
-
-.dot:nth-child(2) {
-  animation-delay: -0.1s;
-}
-
-.dot:nth-child(3) {
-  animation-delay: 0.1s;
-}
-
-@keyframes pulse {
-  0% {
-    transform: scale(0.8);
-    background-color: #5B8E81;
-    box-shadow: 0 0 0 0 rgba(178, 212, 252, 0.7);
-  }
-
-  50% {
-    transform: scale(1.2);
-    background-color: #5B8E81;
-    box-shadow: 0 0 0 10px rgba(178, 212, 252, 0);
-  }
-
-  100% {
-    transform: scale(0.8);
-    background-color: #5B8E81;
-    box-shadow: 0 0 0 0 rgba(178, 212, 252, 0.7);
-  }
-}
 .button-select {
     background-color: #ffffff00;
     color: #5B8E81;
